4d940eb76be8f8f91f0e747638345101123ace3a
eossy1019 <eossy1019@gmail.com>
2022-06-20 14:46:25 +0900

    문의 작업

---
+++ testforgit/src/main/java/com/kh/testforgit/Moon1.java
@@ -0,0 +1,8 @@
+package com.kh.testforgit;
+
+public class Moon1 {
+	public static void main(String[] args) {
+		System.out.println("문일");
+	}
+
+}
---
+++ testforgit/src/main/java/com/kh/testforgit/Moon2.java
@@ -0,0 +1,8 @@
+package com.kh.testforgit;
+
+public class Moon2 {
+	public static void main(String[] args) {
+		System.out.println("문2");
+	}
+
+}
